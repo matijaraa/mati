5263f4d031a325d0aba046ebbcbd9205b7eb34a2
Matias Jara <matijaraa@gmail.com>
2022-07-12 19:24:55 -0300

    Encuentro errores en script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector("title").innerHTML)
+
+console.log('\n')
+
+let nuevoLength = arrayPares.push(20)
+console.log(arrayPares)
+console.log("nuevoLength: ", nuevoLength)
+console.log(arrayPares)
+
